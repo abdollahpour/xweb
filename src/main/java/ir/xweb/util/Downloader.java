25347040d57f12dbf4389e057d8980cea88f7ca3
Hamed Abdollahpour <ha.hamed@gmail.com>
2013-10-07 00:31:30 +0800

    Add downloader class

---
+++ src/main/java/ir/xweb/util/Downloader.java
@@ -0,0 +1,11 @@
+package ir.xweb.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hamed
+ * Date: 10/6/13
+ * Time: 9:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Downloader {
+}
